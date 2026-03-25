1bedca434f1e3e3b2231968cffc2642fc85c4a8e
lforcier-dba <lynnforcier2026@gmail.com>
2026-03-24 23:09:52 -0700

    Enhance dba_tab_par.sql with formatting and compute options

--- dba_tab_par.sql
+++ dba_tab_par.sql
@@ -1,3 +1,12 @@
+col index_name for a45
+col table_name for a45
+col index_owner for a15
+col table_owner for a15
+compute sum of count on report
+break on report
+set linesize 260
+set pagesize 200
+
 select table_owner,tablespace_name, table_name, count(1) count from dba_tab_partitions
 where table_owner not in ('SYS')
 and table_owner = 'OWNER'
